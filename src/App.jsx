9da5e141573c5b8e147b1d9134c36fcb8c9c2fc6
Abhishek Raj <abhishekraj605912@gmail.com>
2025-09-21 03:22:34 +0530

    Added animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import SplitText from "./assets/SpliteText";
+
 function App() {
+  const handleAnimationComplete = () => {
+    console.log("All letters have animated!");
+  };
   return (
     <>
-    <div className="bg-blue-500 flex justify-center align-middle h-full">
-      <p className="text-white">Hello ! I am Abhishek Raj</p>
-    </div>
+    <div className="h-screen flex items-center justify-center bg-blue-400">
+      <SplitText
+        text="Hello! I am Abhishek Raj"
+        className="text-4xl text-white font-semibold text-center"
+        delay={100}
+        duration={0.3}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+        onLetterAnimationComplete={handleAnimationComplete}
+      />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
